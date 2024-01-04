065bd3dcde16652fc1d67c603aea4ccab260bc51
coffee-poter <huns0108@naver.com>
2024-01-05 03:21:01 +0900

    remove conflict1

--- src/main/java/hackerthon/liquor/repository/LiquorRepository.java
+++ src/main/java/hackerthon/liquor/repository/LiquorRepository.java
@@ -5,11 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LiquorRepository extends JpaRepository<Liquor, Long> {
-    List<Liquor> findByCategoryId(Long CategoryId);
-    Optional<Liquor> findByCategoryId(Long id);
-
+    List<Liquor> findByCategoryId(Long categoryId);
+    Optional<Liquor> findById(Long id);
 }
